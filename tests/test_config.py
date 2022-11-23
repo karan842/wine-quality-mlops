0288b023280b921dcbfde86af90dbf12c26c6ba9
Karan <karanshingde@gmail.com>
2022-11-23 15:50:39 +0530

    range defined in json for test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,13 @@
+import pytest
 
+class NotInRangeError(Exception):
+    def __init__(self,message="Value not in range"):
+        # self.inputs = input
+        self.message = message
+        super().__init__(self.message)
 
 def test_generic():
-    a = 2
-    b = 2
-    assert a==b
+    a = 50
+    with pytest.raises(NotInRangeError):
+        if a not in range(10,20):
+            raise NotInRangeError
